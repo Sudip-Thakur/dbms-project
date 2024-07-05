14865500ca9b840f23fa1e65f7e482e41599b766
Sanjeep <sanjeepshrmaa0110@gmail.com>
2024-07-05 22:42:47 +0545

    Mainpage

---
+++ frontend/src/Components/Sidebar/Sidebar.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import './Sidebar.css'
+import home from '../../assets/home.png'
+import subscriptions from '../../assets/subscriptions.png'
+import watch_history from '../../assets/watch_history.png'
+import playlists from '../../assets/playlists.png'
+import like from '../../assets/like.png'
+import jack from '../../assets/jack.png'
+import simon from '../../assets/simon.png'
+import megan from '../../assets/megan.png'
+
+const Sidebar = ({sidebar}) => {
+  return (
+    <div className={`sidebar ${sidebar?"":"small-sidebar"}`}>
+        <div className="sortcut-links">
+            <div className="side-link">
+                <img src={home} alt="" /><p>Home</p>
+            </div>
+            <div className="side-link">
+                <img src={subscriptions} alt="" /><p>Subscriptions</p>
+            </div>
+            <div className="side-link">
+                <img src={watch_history} alt="" /><p>Watch History</p>
+            </div>
+            <div className="side-link">
+                <img src={playlists} alt="" /><p>Playlists</p>
+            </div>
+            <div className="side-link">
+                <img src={like} alt="" /><p>Liked Videos</p>
+            </div>
+            <hr />
+        </div>
+        <div className="subscribed-list">
+            <h3>Subscribed</h3>
+            <div className="side-link">
+                <img src={jack} alt="" /><p>Bei078</p>
+            </div>
+            <div className="side-link">
+                <img src={simon} alt="" /><p>TEC</p>
+            </div>
+            <div className="side-link">
+                <img src={megan} alt="" /><p>Mrbeast</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Sidebar

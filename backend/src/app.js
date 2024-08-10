3a9f3642627f5b657c0c6f867dd83a13b8000931
Sudip-Thakur <sudipthakur643@gmail.com>
2024-08-10 15:38:29 +0545

    updated

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,16 +4,16 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 
-// app.use(cors({
-//   // origin: process.env.CORS_ORIGIN,
-//   origin: 'https://video-hub-beta-ecru.vercel.app',
-//   credentials: true
-// }));
 app.use(cors({
   // origin: process.env.CORS_ORIGIN,
-  origin: 'http://localhost:5173',
+  origin: 'https://video-hub-beta-ecru.vercel.app',
   credentials: true
 }));
+// app.use(cors({
+//   // origin: process.env.CORS_ORIGIN,
+//   origin: 'http://localhost:5173',
+//   credentials: true
+// }));
 
 app.use(express.json({ limit : '16kb'}));
 
